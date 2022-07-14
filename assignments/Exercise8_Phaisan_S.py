242b46a98cfd6ffd813a2562521e203bcb2bb074
Phaisansaiyotha <phaisansaiyotha88@gmail.com>
2022-07-14 22:05:08 +0700

    Create Exercise8_Phaisan_S.py

---
+++ assignments/Exercise8_Phaisan_S.py
@@ -0,0 +1,13 @@
+usernameInput = input("Username :")
+passwordInput = input("Password :")
+if usernameInput == "phaisan" and passwordInput == "4444":
+    print("welcome")
+
+    print("รายการราคาสินค้า")
+    print("ขนม","10")
+    print("ผงซักฟอก","20")
+    print("ถุงเท้า","15")
+    print("ผ้ากันเปื้อน","100")
+    print("ถุงดำ","50")
+    amount = int(input("จำนวนสินค้า"))
+    if amount ==
