0e2766992e5b91c0463dba770afd4fb59fb7faaf
Phaisansaiyotha <phaisansaiyotha88@gmail.com>
2022-07-14 22:07:32 +0700

    Update Exercise8_Phaisan_S.py

--- assignments/Exercise8_Phaisan_S.py
+++ assignments/Exercise8_Phaisan_S.py
@@ -1,13 +1,45 @@
-usernameInput = input("Username :")
-passwordInput = input("Password :")
-if usernameInput == "phaisan" and passwordInput == "4444":
-    print("welcome")
+''''โปรแกรมเลือกซื้อสินค้า'''
+user = input("Username :")
+password = int(input("Password :"))
+if user.upper() == "CUSTOMER" and password == 1234:
+    print("----------------------")
+    print("---Welcom To A shop---")
+    print("please select an option below")
+    water = 10
+    egg = 5
+    apple = 15
+    print("----------------------")
+    print("option 1 = Water x 1  :", water, "BTH")
+    print("option 2 = Egg   x 1  :", egg, "BTH")
+    print("option 3 = Apple x 1  :", apple, "Baht")
+    select = int(input("Option :"))
 
-    print("รายการราคาสินค้า")
-    print("ขนม","10")
-    print("ผงซักฟอก","20")
-    print("ถุงเท้า","15")
-    print("ผ้ากันเปื้อน","100")
-    print("ถุงดำ","50")
-    amount = int(input("จำนวนสินค้า"))
-    if amount ==
+    if select == 1:
+        item = "Water"
+        number = int(input("Enter number of item :"))
+        total = water * number
+
+    elif select == 2:
+        item = "Egg"
+        number = int(input("Enter number of item :"))
+        total = egg * number
+
+
+    elif select == 3:
+        item = "Apple"
+        number = int(input("Enter number of item :"))
+        total = apple * number
+
+    else:
+        item = "Not found"
+        number = 0
+        total = 0
+    print("-----------------------")
+
+    print(item, "---------------", "x", number, "\ntotal =", total, "BTH")
+
+
+elif user.upper() == "CUSTOMER" and password != 1234:
+    print("Your password is incorrect.\n Please try again")
+else:
+    print("Your username is incorrect. \n Please try again")
